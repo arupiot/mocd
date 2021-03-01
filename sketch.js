8a2ff85c787f0e4dc38e7d826f36f2a7ae038bfa
Francesco Anselmo <francesco.anselmo@gmail.com>
2021-03-01 00:30:09 +0000

    correct face scaling

--- sketch.js
+++ sketch.js
@@ -4,6 +4,8 @@ let label = '';
 let myFont;
 let facemesh;
 let predictions = [];
+let aspectX = 1.0;
+let aspectY = 1.0;
 
 const devices = [];
 
@@ -19,6 +21,9 @@ function setup() {
   //   fullscreen(!fs);
   navigator.mediaDevices.enumerateDevices().then(gotDevices);
   video = createCapture(VIDEO);
+  aspectX = windowWidth / video.width;
+  aspectY = windowHeight / video.height;
+  console.log(aspectX, aspectY);
   // video.size(width, height);
   facemesh = ml5.facemesh(video, modelReady);
   facemesh.on("predict", results => {
@@ -35,6 +40,7 @@ function setup() {
 function draw() {
   background(0);
   image(video, 0, 0, width, height);
+  // image(video, 0, 0);
   drawKeypoints();
   fill(255);
   textSize(32);
@@ -43,21 +49,71 @@ function draw() {
 
 // A function to draw ellipses over the detected keypoints
 function drawKeypoints() {
+  aspectX = windowWidth / video.width;
+  aspectY = windowHeight / video.height;
   for (let i = 0; i < predictions.length; i += 1) {
-    // const keypoints = predictions[i].scaledMesh;
-    const keypoints = predictions[i].mesh;
+    const keypoints = predictions[i].scaledMesh;
+    // const keypoints = predictions[i].mesh;
+    // const keypoints = predictions[i].annotations.silhouette;
+    const bbox = predictions[i].boundingBox;
+    const sections = predictions[i].annotations;
+    // console.log(sections);
+    // console.log(bbox.bottomRight[0][0],);
+    const tx = (bbox.bottomRight[0][0]+bbox.topLeft[0][0])/2;
+    const ty = (bbox.topLeft[0][1]+bbox.bottomRight[0][1])/2;
+    const w = bbox.bottomRight[0][0]-bbox.topLeft[0][0]
+    const h = -(bbox.topLeft[0][1]-bbox.bottomRight[0][1])
     // const keypoints = predictions[i];
 
     // Draw facial keypoints.
     for (let j = 0; j < keypoints.length; j += 1) {
       // console.log(keypoints[j]);
+      
       const [x, y] = keypoints[j];
 
       fill(255, 255, 255);
-      ellipse(x, y, 3, 3);
+
+      // ellipse(x+tx, y+ty, 3, 3);
+      // rect(bbox.topLeft[0][0], bbox.topLeft[0][1], w, h);
+      ellipse(x*aspectX, y*aspectY, 3, 3);
+      noFill();
+      rect(bbox.topLeft[0][0]*aspectX, bbox.topLeft[0][1]*aspectX, w*aspectX, h*aspectY);
     }
   }
 }
+
+/*
+leftCheek: 
+leftEyeLower0: 
+leftEyeLower1: 
+leftEyeLower2: 
+leftEyeLower3: 
+leftEyeUpper0: 
+leftEyeUpper1: 
+leftEyeUpper2: 
+leftEyebrowLower: 
+leftEyebrowUpper: 
+lipsLowerInner: 
+lipsLowerOuter: 
+lipsUpperInner: 
+lipsUpperOuter: 
+midwayBetweenEyes: 
+noseBottom: 
+noseLeftCorner: 
+noseRightCorner: 
+noseTip: 
+rightCheek: 
+rightEyeLower0: 
+rightEyeLower1: 
+rightEyeLower2: 
+rightEyeLower3: 
+rightEyeUpper0: 
+rightEyeUpper1: 
+rightEyeUpper2: 
+rightEyebrowLower: 
+rightEyebrowUpper: 
+silhouette: 
+*/
 
 function modelReady() {
   console.log('Model is ready!!!');
@@ -98,6 +154,8 @@ function gotDevices(deviceInfos) {
 }
 
 function windowResized() {
+  aspectX = windowWidth / video.width;
+  aspectY = windowHeight / video.height;
   resizeCanvas(windowWidth, windowHeight);
 }
 
